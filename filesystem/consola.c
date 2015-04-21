b3e9ead76e3a5d2e66038e748f19a738f7213516
javmo <javmo94@gmail.com>
2015-04-21 19:28:45 -0300

    creo el poryecto fileSystem en la carpeta que le corresponde, cualquier cosa diganme a ver si hice cagada todavia no le agarro bien la mano

---
+++ filesystem/consola.c
@@ -0,0 +1,16 @@
+/*
+ * consola.c
+ *
+ *  Created on: 21/4/2015
+ *      Author: utnso
+ */
+
+
+#include <stdio.h>
+
+int main (){
+
+	printf("aca empieza el proyecto filesystem");
+
+	return 0;
+}
