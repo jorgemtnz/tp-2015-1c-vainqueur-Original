ba0ab65b3644d0b78d1ff91863250aa033ff834f
jorgemtnz <jorgeemtnz@gmail.com>
2015-05-10 19:25:42 -0300

    se agrega archivo para codigo que no se usa, y se limpia el codigo no usado en mapeoAmemoria.

--- mapeoAMemoria/main.c
+++ mapeoAMemoria/main.c
@@ -30,24 +30,4 @@ int main(int argc, char **argv) {
 	return 1;
 }
 
-/* Maxi esto no lo borre, lo dejo a tu desicion, saludos.
 
-//printf ("asdasdasdas");
-//crearArchivo("ArchivoDePrueba"); //Crear Archivo Para Mapear de 190 mb
-
- //Variables
- const char * bloqueDeMemoria;
- int direccionArchivo;
- //
- direccionArchivo = open("archivoDePrueba/archivoDePrueba.txt", O_RDONLY);
- if (direccionArchivo == -1) {
- perror("Error opening file for reading");
- exit(EXIT_FAILURE);
- }
- bloqueDeMemoria = mmap (NULL,2e+7,PROT_WRITE, MAP_PRIVATE, direccionArchivo, 0); //2e+7 son 20mb
- //if (bloqueDeMemoria == MAP_FAILED) handle_error("mmap");
- //munmap() para desmapear
-
- return 1;
- }
- */
