d163f046d8e7d5e98256ace76ff0f85c9228c03c
maxivz <maxivz94@yahoo.com>
2015-05-25 01:17:30 -0300

    Cambios a Redireccion IO para mejor lectura

--- Redireccion IO/src/RedireccionIO.c
+++ Redireccion IO/src/RedireccionIO.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "/home/utnso/TpOperativos/mapeoAMemoria/mapeoAMemoria.h"
+//#include "../home/utnso/TpOperativos/mapeoAMemoria/mapeoAMemoria.h"
 
 #define NUMERO_PIPES 2
-#define PIPE_LECTURA_PADRE 0
-#define PIPE_ESCRITURA_PADRE 1
+#define NUMERO_CANALES 2
+#define LECTURA_PADRE 0
+#define ESCRITURA_PADRE 1
 /* Siempre que se usen pipes:
  * 1 para escribir
  * 0 para leer
  */
-#define ESCRIBIR_FD 1
-#define LEER_FD 0
-#define PADRE_LECTURA_FD (pipes[PIPE_LECTURA_PADRE][LEER_FD])
-#define PADRE_ESCRITURA_FD (pipes[PIPE_ESCRITURA_PADRE][ESCRIBIR_FD])
-#define HIJO_LECTURA_FD (pipes[PIPE_ESCRITURA_PADRE][LEER_FD])
-#define HIJO_ESCRITURA_FD (pipes[PIPE_LECTURA_PADRE][ESCRIBIR_FD])
+#define ESCRIBIR 1
+#define LEER 0
+#define CANAL_LECTURA_PADRE (pipes[LECTURA_PADRE][LEER])
+#define CANAL_ESCRITURA_PADRE (pipes[ESCRITURA_PADRE][ESCRIBIR])
+#define CANAL_LECTURA_HIJO (pipes[ESCRITURA_PADRE][LEER])
+#define CANAL_ESCRITURA_HIJO (pipes[LECTURA_PADRE][ESCRIBIR])
 
-int pipes[NUMERO_PIPES][2];
+int pipes[NUMERO_PIPES][NUMERO_CANALES];
 
 //void ejecutarRedireccionandoIO (entrada,salida,funcion);
 
-int main( entrada, salida, funcion) {
+int main() {
 	//Definido para el mapeo
-	char* ptrComienzoMemoriaMapeada;
-	int * ptrTamanioPagina;
+	//char* ptrComienzoMemoriaMapeada;
+	//int * ptrTamanioPagina;
 	//Inicializacion pipes de lectura y escritura
-	pipe(pipes[PIPE_ESCRITURA_PADRE]);
-	pipe(pipes[PIPE_LECTURA_PADRE]);
-	mapeoAmemoria("ArchivoPrueba.txt", ptrComienzoMemoriaMapeada, ptrTamanioPagina);
+	pipe(pipes[ESCRITURA_PADRE]);
+	pipe(pipes[LECTURA_PADRE]);
+	/*mapeoAmemoria("ArchivoPrueba.txt", ptrComienzoMemoriaMapeada,
+			ptrTamanioPagina);*/
 	if (fork() == 0) {
 		//Proceso Hijo
 		/*Asigno el proceso "bc" a argv, bc permite hacer cuentas y demas
@@ -36,14 +38,14 @@ int main( entrada, salida, funcion) {
 		 */
 		char *argv[] = { "/usr/bin/bc", "-q", 0 };
 		//Duplico el fd de lectura del hijo al fd de la entrada standar
-		if (dup2(HIJO_LECTURA_FD, STDIN_FILENO) == -1) {
+		if (dup2(CANAL_LECTURA_HIJO, STDIN_FILENO) == -1) {
 			perror(
 					"[ERROR] Funcion DUP2: No se pudo duplicar el File Descriptor para la entrada\n");
 			exit(-1);
 		}
 		//Duplico el fd de escritura del hijo al fd de la salida standar
-		dup2(HIJO_ESCRITURA_FD, STDOUT_FILENO);
-		if (dup2(HIJO_LECTURA_FD, STDIN_FILENO) == -1) {
+
+		if (dup2(CANAL_ESCRITURA_HIJO, STDOUT_FILENO) == -1) {
 			perror(
 					"[ERROR] Funcion DUP2: No se pudo duplicar el File Descriptor para la salida\n");
 			exit(-1);
@@ -51,10 +53,10 @@ int main( entrada, salida, funcion) {
 
 		//Cierro los pipes que no se van a usar
 
-		close(PADRE_LECTURA_FD);
-		close(PADRE_ESCRITURA_FD);
-		close(HIJO_LECTURA_FD);
-		close(HIJO_ESCRITURA_FD);
+		close(CANAL_LECTURA_PADRE);
+		close(CANAL_ESCRITURA_PADRE);
+		close(CANAL_LECTURA_HIJO);
+		close(CANAL_ESCRITURA_HIJO);
 
 		//Ejecuto proceso en argv[0]
 		execv(argv[0], argv);
@@ -65,14 +67,14 @@ int main( entrada, salida, funcion) {
 		int contador = 0;
 
 		/* Cierro los pipes que le hijo no va a usar */
-		close(HIJO_ESCRITURA_FD);
-		close(HIJO_LECTURA_FD);
+		close(CANAL_ESCRITURA_HIJO);
+		close(CANAL_LECTURA_HIJO);
 
 		// Escribo a la entrada del hijo
-		write(PADRE_ESCRITURA_FD, "1000 * 8\n", 9);
+		write(CANAL_ESCRITURA_PADRE, "3000 * 8\n", 9);
 
 		// Leo de la salida standar del hijo
-		contador = read(PADRE_LECTURA_FD, buffer, sizeof(buffer) - 1);
+		contador = read(CANAL_LECTURA_PADRE, buffer, sizeof(buffer) - 1);
 		if (contador >= 0) {
 			buffer[contador] = 0;
 			printf("%s", buffer);
