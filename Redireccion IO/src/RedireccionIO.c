5e785cba163747351d4094e62fd8c2cbd40f7713
maxivz <maxivz94@yahoo.com>
2015-05-27 00:28:05 -0300

    Cambios menores a redireccion IO para mejor organizacion, limpiado un poco el codigo

--- Redireccion IO/src/RedireccionIO.c
+++ Redireccion IO/src/RedireccionIO.c
@@ -4,37 +4,17 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
-/*
- #define NUMERO_PIPES 2
- #define NUMERO_CANALES 2
- #define LECTURA_PADRE 0
- #define ESCRITURA_PADRE 1
- Siempre que se usen pipes:
- 1 para escribir
- 0 para leer
-
- #define ESCRIBIR 1
- #define LEER 0
- #define CANAL_LECTURA_PADRE (pipes[LECTURA_PADRE][LEER])
- #define CANAL_ESCRITURA_PADRE (pipes[ESCRITURA_PADRE][ESCRIBIR])
- #define CANAL_LECTURA_HIJO (pipes[ESCRITURA_PADRE][LEER])
- #define CANAL_ESCRITURA_HIJO (pipes[LECTURA_PADRE][ESCRIBIR])
-
- int pipes[NUMERO_PIPES][NUMERO_CANALES];
- */
-//Si no se usan los pipes los voy a borrar despues para optimizar mas el codigo
-//void ejecutarRedireccionandoIO (entrada,salida,funcion);
-int ejecutarScriptRedireccionandoIO(char *, char *, char *, char *);
+#include "RedireccionIO.h"
 
 int main()
 {
 	printf("Ejecutando \n");
-	if(ejecutarScriptRedireccionandoIO("Salida.txt", "Entrada.txt", "/usr/bin/sort","sort") == 0)
+	if(ejecutarScriptRedireccionandoIO("Salida.txt", "Entrada.txt", "/usr/bin/sort","") == 0)
 	{
 		printf("Ejecucion correcta \n");
-		return 0;
+
 	}
+	return 0;
 }
 
 int ejecutarScriptRedireccionandoIO(char * dirArchivoSalida,
@@ -51,7 +31,6 @@ int ejecutarScriptRedireccionandoIO(char * dirArchivoSalida,
 		fdArchivoSalida = open(dirArchivoSalida, O_RDWR | O_CREAT);
 		//Abro el archivo que voy a usar como entrada
 		fdArchivoEntrada = open(dirArchivoEntrada, O_RDONLY);
-		char *argv[] = { dirScript};
 		//Duplico el fd de lectura del hijo al fd de la entrada standar
 		if (dup2(fdArchivoEntrada, STDIN_FILENO) == -1) {
 			perror(
---
+++ Redireccion IO/src/RedireccionIO.h
@@ -0,0 +1,22 @@
+/*
+ #define NUMERO_PIPES 2
+ #define NUMERO_CANALES 2
+ #define LECTURA_PADRE 0
+ #define ESCRITURA_PADRE 1
+ Siempre que se usen pipes:
+ 1 para escribir
+ 0 para leer
+
+ #define ESCRIBIR 1
+ #define LEER 0
+ #define CANAL_LECTURA_PADRE (pipes[LECTURA_PADRE][LEER])
+ #define CANAL_ESCRITURA_PADRE (pipes[ESCRITURA_PADRE][ESCRIBIR])
+ #define CANAL_LECTURA_HIJO (pipes[ESCRITURA_PADRE][LEER])
+ #define CANAL_ESCRITURA_HIJO (pipes[LECTURA_PADRE][ESCRIBIR])
+
+ int pipes[NUMERO_PIPES][NUMERO_CANALES];
+ */
+//Si no se usan los pipes los voy a borrar despues para optimizar mas el codigo
+//void ejecutarRedireccionandoIO (entrada,salida,funcion);
+
+int ejecutarScriptRedireccionandoIO(char *, char *, char *, char *); //Recibe un archivo de entrada, uno de salida, direccion del script y nombre del script a ejecutar.
