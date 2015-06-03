8a33f76b033e9665d82a4fec0557c7a57dd9d0fd
maxivz <maxivz94@yahoo.com>
2015-06-03 01:14:07 -0300

    Modificado Redireccion IO para que utilize algo mapeado a memoria como entrada, agregado mapeo a memoria a las commons en la carpeta del equipo

--- Redireccion IO/src/RedireccionIO.h
+++ Redireccion IO/src/RedireccionIO.h
@@ -1,21 +1,20 @@
-/*
- #define NUMERO_PIPES 2
+ #define NUMERO_PIPES 1
  #define NUMERO_CANALES 2
  #define LECTURA_PADRE 0
  #define ESCRITURA_PADRE 1
- Siempre que se usen pipes:
+ /*Siempre que se usen pipes:
  1 para escribir
  0 para leer
-
+*/
  #define ESCRIBIR 1
  #define LEER 0
- #define CANAL_LECTURA_PADRE (pipes[LECTURA_PADRE][LEER])
+ //#define CANAL_LECTURA_PADRE (pipes[LECTURA_PADRE][LEER])
  #define CANAL_ESCRITURA_PADRE (pipes[ESCRITURA_PADRE][ESCRIBIR])
  #define CANAL_LECTURA_HIJO (pipes[ESCRITURA_PADRE][LEER])
- #define CANAL_ESCRITURA_HIJO (pipes[LECTURA_PADRE][ESCRIBIR])
+// #define CANAL_ESCRITURA_HIJO (pipes[LECTURA_PADRE][ESCRIBIR])
 
  int pipes[NUMERO_PIPES][NUMERO_CANALES];
- */
+
 //Si no se usan los pipes los voy a borrar despues para optimizar mas el codigo
 //void ejecutarRedireccionandoIO (entrada,salida,funcion);
 
