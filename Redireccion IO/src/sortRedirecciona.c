174443cdc32a4dd64d151a892f1a000fa6195497
lucasVoboril <lucasvoboril@gmail.com>
2015-06-04 16:09:56 -0300

    Creamos libreria compartida delEquipo que contenga funciones compartidas, incluye sockets y mapeoAMemoria

--- Redireccion IO/src/sortRedirecciona.c
+++
@@ -1,8 +0,0 @@
-/*
- * sortRedirecciona.c
- *
- *  Created on: 27/5/2015
- *      Author: utnso
- */
-
-
--- socket/Debug/src/subdir.mk
+++ socket/Debug/src/subdir.mk
@@ -4,12 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/mapeoAMemoria.c \
 ../src/sockets.c 
 
 OBJS += \
+./src/mapeoAMemoria.o \
 ./src/sockets.o 
 
 C_DEPS += \
+./src/mapeoAMemoria.d \
 ./src/sockets.d 
 
 
