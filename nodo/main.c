5cab6c081aa7a07dbeac4d38e2d1f0797652d3cb
jorgemtnz <jorgeemtnz@gmail.com>
2015-06-07 15:25:45 -0300

    resuelto localmente que no agregue los metadata del eclipse

--- nodo/main.c
+++ nodo/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-/*
+/*se comenta para de esta forma tener un solo main, estoy probando que no suba los metadata
 
 int main(int argc, char **argv) {
 	return 0;
