bf96992a9c26f1345eb5d9002a3676030d4225ad
jorgemtnz <jorgeemtnz@gmail.com>
2015-06-07 15:37:19 -0300

    se suben archivo modificados, y no aparece ya el  metadata para agregar

--- nodo/main.c
+++ nodo/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /*se comenta para de esta forma tener un solo main, estoy probando que no suba los metadata
-
+se sigue moficiando un poco para probar
 int main(int argc, char **argv) {
 	return 0;
 }
---
+++ nodo/prueba.c
@@ -0,0 +1,8 @@
+/*
+ * prueba.c
+ *
+ *  Created on: 7/6/2015
+ *      Author: utnso
+ */
+
+//se hace para probar que no agregue los metadatas
