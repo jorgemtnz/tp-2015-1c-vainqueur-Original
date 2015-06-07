0001d52561ae163034fde779c1c3432a5aa31775
Lucas Voboril <lucasvoboril@gmail.com>
2015-06-07 20:16:37 -0300

    Arreglo unos detalles del seleccionarSockets, los argumentos segundos y microsegundos son punteros

--- delEquipo/sockets/sockets.c
+++ delEquipo/sockets/sockets.c
@@ -8,10 +8,12 @@
 #include <arpa/inet.h>  //INADDR_ANY
 #include <unistd.h>     //close
 
-int  crearSocket(){
+int crearSocket()
+{
 	// Usado por servidor y por cliente
 	int fileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
-	if(fileDescriptor == -1){
+	if (fileDescriptor == -1)
+	{
 		perror("[ERROR] Funcion socket\n");
 		exit(-1);
 	}
@@ -19,68 +21,81 @@ int  crearSocket(){
 	return fileDescriptor;
 }
 
-void asociarSocket(int sockfd, int puerto){
+void asociarSocket(int sockfd, int puerto)
+{
 	// Usado por el Servidor
 	struct sockaddr_in miDireccion;
 	// Seteamos los valores a la estructura miDireccion
-	miDireccion.sin_family      = AF_INET;
-	miDireccion.sin_port        = htons(puerto);
+	miDireccion.sin_family = AF_INET;
+	miDireccion.sin_port = htons(puerto);
 	miDireccion.sin_addr.s_addr = 0; // htonl(INADDR_ANY); // Usa mi direccion IP
 	memset(&(miDireccion.sin_zero), '\0', 8);
 
 	// Resolvemos el ADDRES already in use
 	int reuse = 1;
-	    if(setsockopt(sockfd,SOL_SOCKET, SO_REUSEADDR, (char*)&reuse, sizeof(int)) == -1){
-	      perror("[ERROR] No es posible reusar el socket\n");
-	      exit(1);
-	    }
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*) &reuse,
+			sizeof(int)) == -1)
+	{
+		perror("[ERROR] No es posible reusar el socket\n");
+		exit(1);
+	}
 
 	// Funcion bind
-	int posibleError = bind(sockfd, (struct sockaddr *)&miDireccion, sizeof(struct sockaddr));
+	int posibleError = bind(sockfd, (struct sockaddr *) &miDireccion,
+			sizeof(struct sockaddr));
 
-	if(posibleError == -1){
+	if (posibleError == -1)
+	{
 		perror("[ERROR] Funcion BIND: No se pudo asociar con el puerto\n");
 		exit(-1);
 	}
 
-	printf("[OK] Funcion BIND. Vinculada al puerto [ %d ]\n",puerto);
+	printf("[OK] Funcion BIND. Vinculada al puerto [ %d ]\n", puerto);
 }
 
-void conectarSocket(int sockfd,char* ip_Destino, int puerto){
+void conectarSocket(int sockfd, char* ip_Destino, int puerto)
+{
 	// Usado por el cliente
 	struct sockaddr_in datosServidor;
 	// Seteo estructura dest_addr
-	datosServidor.sin_family      = AF_INET;
-	datosServidor.sin_port        = htons(puerto);
+	datosServidor.sin_family = AF_INET;
+	datosServidor.sin_port = htons(puerto);
 	datosServidor.sin_addr.s_addr = inet_addr(ip_Destino);
 	memset(&(datosServidor.sin_zero), '\0', 8);
 
-	int posibleError = connect(sockfd, (struct sockaddr *)&datosServidor, sizeof(struct sockaddr));
-	if(posibleError ==-1){
+	int posibleError = connect(sockfd, (struct sockaddr *) &datosServidor,
+			sizeof(struct sockaddr));
+	if (posibleError == -1)
+	{
 		perror("[ERROR] Funcion connect\n");
 		exit(-1);
 	}
 	printf("[OK] Funcion CONNECT\n");
 }
 
-void  escucharSocket(int sockfd, int conexionesEntrantesPermitidas){
+void escucharSocket(int sockfd, int conexionesEntrantesPermitidas)
+{
 	// Usado por el Servidor
-	int posibleError = listen(sockfd,conexionesEntrantesPermitidas);
-	if(posibleError == -1){
+	int posibleError = listen(sockfd, conexionesEntrantesPermitidas);
+	if (posibleError == -1)
+	{
 		perror("[ERROR] Funcion listen\n");
 		exit(-1);
 	}
-	printf("[OK] Funcion LISTEN. Admite [ %d ] conexiones entrantes\n",conexionesEntrantesPermitidas);
+	printf("[OK] Funcion LISTEN. Admite [ %d ] conexiones entrantes\n",
+			conexionesEntrantesPermitidas);
 }
 
-int  aceptarConexionSocket(int sockfd){
+int aceptarConexionSocket(int sockfd)
+{
 	// Usado por el servidor
 	struct sockaddr_storage cliente;
 	unsigned int addres_size = sizeof(cliente);
 
-	int otroFD = accept(sockfd,(struct sockaddr*) &cliente, &addres_size);
+	int otroFD = accept(sockfd, (struct sockaddr*) &cliente, &addres_size);
 
-	if(otroFD == -1){
+	if (otroFD == -1)
+	{
 		perror("[ERROR] Funcion accept");
 		exit(-1);
 	}
@@ -89,49 +104,69 @@ int  aceptarConexionSocket(int sockfd){
 	return otroFD;
 }
 
-void enviarPorSocket(int fdCliente,const void *msg, int len){
+void enviarPorSocket(int fdCliente, const void *msg, int len)
+{
 	// Usado por servidor y cliente
 	int bytes_enviados = send(fdCliente, msg, len, 0);
 
-	if(bytes_enviados == -1){
+	if (bytes_enviados == -1)
+	{
 		perror("[ERROR] Funcion recv\n");
-		exit(-1);           }
+		exit(-1);
+	}
 
-	if(bytes_enviados != len){
+	if (bytes_enviados != len)
+	{
 		perror("[ERROR] No se envio la cadena entera\n");
 	}
 }
 
-int recibirPorSocket(int fdCliente, void *buf, int len){
+int recibirPorSocket(int fdCliente, void *buf, int len)
+{
 	// Usado por servidor y cliente
-	int bytes_recibidos = recv(fdCliente, buf,len, 0);
+	int bytes_recibidos = recv(fdCliente, buf, len, 0);
 
-	if(bytes_recibidos == -1){
+	if (bytes_recibidos == -1)
+	{
 		perror("[ERROR] Funcion recv\n");
-		exit(1);}
+		exit(1);
+	}
 	return bytes_recibidos;
 }
 
-void cerrarSocket(int sockfd){
+void cerrarSocket(int sockfd)
+{
 	// Usado por servidor y cliente
 	// Esto impedirá más lecturas y escrituras al socket. Cualquiera que intente leer o
 	// escribir sobre el socket en el extremo remoto recibirá un error.
 	close(sockfd);
 }
 
-void seleccionarSocket(int maxNumDeFD, fd_set *fdListoLectura, fd_set *fdListoEscritura,fd_set *fdListoEjecucion, int segundos, int miliSegundos){
+void seleccionarSocket(int maxNumDeFD, fd_set *fdListoLectura,
+		fd_set *fdListoEscritura, fd_set *fdListoEjecucion, int* segundos,
+		int* miliSegundos)
+{
 	struct timeval tv;
 	// Esta estructura de tiempo de permite establecer un período máximo de espera.
 	// Si segundos = 0 y miliSegundos = 0 => regresará inmediatamente después de interrogar
 	// a todos tus file descriptor
 	// Si segundos = NULL y miliSegundos = NULL => espera infinitamente
-	int microsegundos = miliSegundos * 1000;
-	tv.tv_sec = segundos;
-	tv.tv_usec = microsegundos;
+	if (segundos == NULL || miliSegundos == NULL)
+	{
+		tv.tv_sec  = segundos;
+		tv.tv_usec = miliSegundos;
+	}
+	else
+	{
+		int microsegundos = *miliSegundos * 1000;
+		tv.tv_sec  = *segundos;
+		tv.tv_usec = *microsegundos;
+	}
 
-	int posibleError = select( (maxNumDeFD+1), fdListoLectura, fdListoEscritura, fdListoEjecucion,&tv);
+	int posibleError = select((maxNumDeFD + 1), fdListoLectura, fdListoEscritura, fdListoEjecucion, &tv);
 
-	if(posibleError == -1){
+	if (posibleError == -1)
+	{
 		perror("[ERROR] Funcion select\n");
 		exit(-1);
 	}
