60a234f1b7868061fd12eeda7b09d5f4663de1db
jorgemtnz <jorgeemtnz@gmail.com>
2015-06-14 19:13:52 -0300

    se agregan funciones de creacion

--- job/src/jobFuncionesCreacion.c
+++ job/src/jobFuncionesCreacion.c
@@ -9,9 +9,33 @@ t_solicitudDeTrabajo* crearSolicitudDeTrabajo() {
 						"Job>..>SolicitudDeTrabajo");
 		exit(-1);
 	} else {
-		ptrSolicitudDeTrabajo->combiner= NO;
-		ptrSolicitudDeTrabajo->archvATrabajar='\0';
-		ptrSolicitudDeTrabajo->funcion='\0';
+		ptrSolicitudDeTrabajo->combiner = NO;
+		ptrSolicitudDeTrabajo->archvATrabajar = '\0';
+		ptrSolicitudDeTrabajo->funcion = '\0';
 		return ptrSolicitudDeTrabajo;
 	}
 }
+
+t_relacionNodoBloque* crearRelacionNodoBloque() {
+	t_relacionNodoBloque* ptrRelacionNodoBloque;
+
+	if (malloc(sizeof(t_relacionNodoBloque)) == NULL) {
+		perror(
+				"No se aloco memoria para RelacionNodoBloque pertenece a la estructura "
+						"Job>..>RelacionNodoBloque");
+		exit(-1);
+	} else {
+		ptrRelacionNodoBloque->numeroBloque = 0;
+		ptrRelacionNodoBloque->numeroNodo = 0;
+		return ptrRelacionNodoBloque;
+	}
+
+}
+
+t_relacionNodoArchTemp* crearRelacionNodoArchTemp() {
+	t_relacionNodoArchTemp relacionNodoArchTemp;
+	if(malloc(sizeof(t_relacionNodoBloque))==NULL){
+
+	}
+}
+
