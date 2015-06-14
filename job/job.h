b088a58abba6cb790ac2ca6266fc6f7dc064530d
javmo <javmo94@gmail.com>
2015-06-14 04:04:36 -0300

    Estructuras Job

---
+++ job/job.h
@@ -0,0 +1,43 @@
+/*
+ * job.h
+ *
+ *  Created on: 14/6/2015
+ *      Author: utnso
+ */
+#include <commons/collections/list.h>
+
+typedef struct SolicitudDeTrabajo{
+	char* archvATrabajar;
+	char* funcion;
+	int combiner;
+}SolicitudDeTrabajo;
+
+typedef struct RelacionNodoBloque{
+	int numeroNodo;
+	int numeroBloque;
+
+}RelacionNodoBloque;
+
+typedef struct RealcionNodoArchTemp{
+	int numeroNodo;
+	char* nombreArchvTemp;
+}RelacionNodoArchTemp;
+
+typedef struct TareaMap{//cada tarea asemeja a un hilo (en este caso hiloMapper)
+	int fdHilo;
+	t_list* listaRelacionNodoBloque;
+	char* dirMapExec;
+}TareaMap;
+
+typedef struct TareaReduce{
+	int fdHilo;
+	t_list* listaRelacionNodoArchTemp;
+	char* dirReduceExec;
+}TareaReduce;
+
+typedef struct Job {
+	t_list* listasTareasMap;
+	t_list* listasTareasReduce;
+	SolicitudDeTrabajo* solicitud;
+	char* dirArchvConfig;
+};
