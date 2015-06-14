44d3e89953ebc5763d1b0ce0112cd99c7afb1bfc
lucasVoboril <lucasvoboril@gmail.com>
2015-06-14 20:51:52 -0300

    Arreglo todos los headers, me olvide del nodo

---
+++ nodo/src/nodo.h
@@ -0,0 +1,58 @@
+#ifndef NODO_H_
+#define NODO_H_
+
+// +++++++++++++++++++++++++++++++++++++++ Includes +++++++++++++++++++++++++++++++++++++
+#include <stdio.h>
+#include <stdlib.h>
+#include <sockets/sockets.h>
+#include <redireccionIO/redireccionIO.h>
+#include <string.h>
+#include <src/commons/config.h>
+
+// +++++++++++++++++++++++++++++++++++++++ Define +++++++++++++++++++++++++++++++++++++
+#define RUTACONFIGNODO "/"
+#define RUTAMAP "blabl/blabla"
+#define EJECUCIONOK 1
+#define EJECUCIONFALLO -1
+#define NODO_NUEVO 1
+#define NODO_NO_NUEVO 0
+
+// +++++++++++++++++++++++++++++++++++++++ Estructuras +++++++++++++++++++++++++++++++++++++
+typedef struct nodo {
+	int idNodo;
+	int esNuevo; 	// 1 si es nuevo, 0 si no;
+	short estado; 	//1 disponible, 0 ocupado;
+	void * bloques;
+} nodo_t;
+
+// +++++++++++++++++++++++++++++++++++++++ Prototipos +++++++++++++++++++++++++++++++++++++
+void* getBloque(int numeroBloque);	// Falta implementar
+void* setBloque();					// Falta implementar
+void* getFileContent();				// Falta implementar
+
+// Nodo.c
+void levantarArchivoConfiguracion();
+int informarEjecucionAJob(nodo_t* datosDelNodo,
+		char* ipJob, int puertoJob,	int comoTerminoLaEjecucion);
+int ejecutarReduce(int soportaCombiner); // Falta iplementar
+int ejecutarMap(char * nombreArchivoTemporal, char * ptrDireccionMapeo);
+void conectarNodo(nodo_t* datosDelNodo);
+//main
+
+// Funciones Constructoras
+
+// Funciones Destructoras
+
+// Funciones Auxiliares
+
+// +++++++++++++++++++++++++++++++++++ Variables Globales +++++++++++++++++++++++++++++++++++
+int vg_puerto_FS,
+	vg_nodo_Nuevo,
+	vg_puerto_Nodo;
+
+char *vg_ip_FS,
+	 *vg_archivo_Bin,
+	 *vg_dirTemp,
+	 *vg_ip_Nodo;
+
+#endif /* NODO_H_ */
