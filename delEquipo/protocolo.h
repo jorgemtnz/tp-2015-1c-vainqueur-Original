bfed08b195c6ebd118e07f1f6d6011466550ae37
SantiBalbiani <santiagobalbiani@yahoo.com>
2015-06-16 09:56:43 -0300

    Creo archivo de cabecera de protocolo
    Creo archivo de cabecera de protocolo

---
+++ delEquipo/protocolo.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+//Protocolo.h
+#IFNDEF __protocolo__
+
+#define	ARCHIVO_CARGA_INICIAL	0
+#define	ESCRITURA_BLOQUE	1
+#define	INFORMAR_UBICACION_ARCHIVOS	2
+#define	SOLICITUD_JOB	3
+#define	RTA_SOLICITUD_JOB	4
+#define	NOTIFICACION_RESUL	5
+#define	EJECUTAR_MAP	6
+#define	EJECUTAR_RED	7
+#define	SOLICITUD_ARCHIVO	8
+#define	RTA_SOLICITUD_ARCHIVO	9
+
+#ENDIF
+// fin protocolo.h
