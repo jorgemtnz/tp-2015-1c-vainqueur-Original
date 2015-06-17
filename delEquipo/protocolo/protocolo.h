f10df4b05707fb19057207561459748c19e6c0f6
SantiBalbiani <santiagobalbiani@yahoo.com>
2015-06-17 11:47:07 -0300

    Se agrega prototipo funcion prepararParaEnviar
    Se agrega prototipo funcion prepararParaEnviar

--- delEquipo/protocolo/protocolo.h
+++ delEquipo/protocolo/protocolo.h
@@ -24,7 +24,14 @@
 // El separador del protocolo
 const char * SEPARADOR = "$";
 
-// Función polimórfica que retorna el buffer contenido en la estructura del protocolo dado
+// Función polimórfica que retorna el buffer contenido en la estructura del protocolo dado.
 void * obtenerEstructura(char *);
+
+// Función polimórfica que recibe como primer parametro los datos que se van a enviar por socket
+//con una estructura específica.
+// El segundo parametro que recibe es el protocolo que se ubicará delante del string devuelto.
+// devuelve un string con cada valor de esa estructura separado por un separador. El primer byte del
+// string devuelto es el protocolo
+char * prepararParaEnviar(void *, int);
 
 #endif /* PROTOCOLO_H_ */
