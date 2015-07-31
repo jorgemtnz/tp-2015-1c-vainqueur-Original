7304ac2b67225dd108344fdcd8e73c6c1b6cc847
Lucas Voboril <lucasvoboril@gmail.com>
2015-07-31 14:53:22 -0300

    sockets.h agrego prototipos

--- delEquipo/sockets/sockets.h
+++ delEquipo/sockets/sockets.h
@@ -28,39 +28,33 @@ typedef struct {
 } t_equipo;
 
 typedef struct {
-
 	fd_set master;
 	fd_set temp;
 	int listener;
 	int maxSock;
 	char* buffer;
 	int buffersize;
-
 } t_struct_select;
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Funciones
-int  crearSocket();
-int  enviarPorSocket(int fdCliente,const void *msg, int *len);
-int  recibirPorSocket(int fdCliente, void *buf, int len);		// Retorna la cantidad de bytes recibidos
-void comprobarDesconexion(int bytes);
-void cerrarSocket(int sockfd);
-void seleccionarSocket(int maxNumDeFD, fd_set *fdListoLectura, fd_set *fdListoEscritura,
-					   fd_set *fdListoEjecucion, int* segundos, int* miliSegundos);
-int recv_timeout(int s , int timeout);
-struct sockaddr_in especificarSocketInfo(char* direccion, int puerto);
-int recibirMensaje(int unSocket, void** buffer);
+int recv_timeout(int s, int timeout) ;
+struct sockaddr_in especificarSocketInfo(char* direccion, int puerto) ;
+void asociarSocket(int sockfd, int puerto) ;
+int conectarSocket(int sockfd, char* ip_Destino, int puerto);
+int conectarSocketPorPrimeraVez(int sockfd, char* ip_Destino, int puerto) ;
+void escucharSocket(int sockfd, int conexionesEntrantesPermitidas) ;
+int aceptarConexionSocket(int sockfd) ;
+int enviarPorSocket(int fdCliente, const void *msg, int *len) ;
+int recibirPorSocket(int fdCliente, void *buf, int len) ;
+void cerrarSocket(int sockfd) ;
+void seleccionarSocket(int maxNumDeFD, fd_set *fdListoLectura,
+		fd_set *fdListoEscritura, fd_set *fdListoEjecucion, int* segundos,
+		int* milisegundos) ;
+int recibirMensaje(int unSocket, void** buffer) ;
 int recibirHeader(int unSocket, Header* header) ;
 int recibirDatos(int unSocket, Header header, void** buffer);
-int mandarMensaje(int unSocket, int8_t tipo, int tamanio, void *buffer);
-
-
-// Solo para el servidor
-void asociarSocket(int sockfd, int puerto);
-void escucharSocket(int sockfd, int conexionesEntrantesPermitidas);
-int  aceptarConexionSocket(int sockfd);
-// Solo para el ciente
-int conectarSocket(int sockfd,char* ip_destino, int puerto);
-int conectarSocketPorPrimeraVez(int sockfd, char* ip_Destino, int puerto);
+int mandarMensaje(int unSocket, int8_t tipo, int tamanio, void *buffer) ;
+int getSocketChanged(t_struct_select* params) ;
 
 #endif /* SOCKETS_H_ */
